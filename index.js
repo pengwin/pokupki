a74ef65a3c1b879cc8098378c02f0f3ffabac49c
Ivan Kruchkov <pengwin4@gmail.com>
2017-06-28 12:08:50 +0300

    Add database support

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
 const fs = require('fs');
 const path = require('path');
+const pg = require('pg');
+const url = require('url');
 
 const staticPath = './public';
+const port = process.env.PORT || 3000;
+const databaseUrl = process.env.DATABASE_URL || 3000;
 
 function getFilePath(url) {
   if (url === '/') {
-    url = 'index.html';  
+    url = 'index.html';
   }
   return path.join(staticPath, url);
 }
@@ -35,7 +38,7 @@ function handleFileRequest(request, response) {
   let filePath = getFilePath(request.url);
   let contentType = getContentType(filePath);
 
-  fs.readFile(filePath, function (error, content) {
+  fs.readFile(filePath, (error, content) => {
     if (error) {
       if (error.code == 'ENOENT') {
         response.writeHead(404, 'NOT FOUND');
@@ -53,8 +56,57 @@ function handleFileRequest(request, response) {
   });
 }
 
+function handleApiRequest(pool, request, response) {
+  pool.connect().then(client => {
+    client.query('SELECT * FROM test_table')
+      .then(res => {
+        //client.release()
+        response.end(JSON.stringify({ results: res.rows }), 'utf-8');
+      })
+      .catch(err => {
+        //client.release();
+        if (err) {
+          console.error(err);
+          response.writeHead(500);
+          response.end(`Query error: ${e.message}, ${e.stack}`);
+          response.end();
+          return;
+        }
+      });
+  }).catch(err => {
+    if (err) {
+      console.error(err);
+      response.writeHead(500);
+      response.end(`Connect error: ${err.message}, ${err.stack}`);
+      response.end();
+      return;
+    }
+  });
+}
+
+const params = url.parse(databaseUrl);
+const auth = params.auth.split(':');
+
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: true,
+  max: 20
+};
+
+const pool = new pg.Pool(config);
+
 const server = http.createServer((request, response) => {
   console.log(`Handle request ${request.url}`);
+
+  if (request.url.indexOf('/api') > -1) {
+    handleApiRequest(pool, request, response);
+    return;
+  }
+
   handleFileRequest(request, response);
 });
 
